08fc4bdb45ddf60c46b85435d65b2ff6dcc7f07f
Orlando Perez <56610115+o-perez@users.noreply.github.com>
2023-04-11 21:07:44 -0400

    Fixing up carousel

--- PerezSoftwareDevelopment/screens/generalFrame.php
+++ PerezSoftwareDevelopment/screens/generalFrame.php
@@ -2,7 +2,7 @@
 <body>
 <div class="carousel">
     
-    <button class="carousel__button carousel__button--left is-hidden"><</button>
+    <!-- <button class="carousel__button carousel__button--left is-hidden"><</button> -->
 
     <div class="carousel__track-container">
         <ul class="carousel__track">
@@ -12,19 +12,39 @@
                 </a>
             </li>
 
-            <li class="carousel__slide">
+            <li class="carousel__slide currentSlide">
                 <a>
                     <img src="../images/showcaseImg/departments.jpg" id="departments img" alt="departments" class="carousel__image"><h2>Our departments:</h2>
                 </a>
             </li>   
+
+            <li class="carousel__slide currentSlide">
+                <a>
+                    <img src="../images/showcaseImg/location.jpg" id="location img" alt="locations" class="carousel__image"><h2>Our locations:</h2>
+                </a>
+            </li> 
+
+            <li class="carousel__slide currentSlide">
+                <a>
+                    <img src="../images/showcaseImg/offers.jpg" id="offers img" alt="offers" class="carousel__image"><h2>Our offers:</h2>
+                </a>
+            </li> 
+
+            <li class="carousel__slide currentSlide">
+                <a>
+                    <img src="../images/showcaseImg/pricing.png" id="pricing img" alt="pricing" class="carousel__image"><h2>Get estimate:</h2>
+                </a>
+            </li> 
         </ul>    
     </div>
 
-    <button class="carousel__button carousel__button--rigth is-hidden">></button>
+    <!-- <button class="carousel__button carousel__button--rigth is-hidden">></button> -->
 
     <div class="carousel_nav"> 
         <!--Shortcut to create this button button.carousel_indicator*cuantity needed (ex: 3) -->
         <button class="carousel_indicator currentSlide"></button>
+        <button class="carousel_indicator"></button>
+        <button class="carousel_indicator"></button>
         <button class="carousel_indicator"></button>
         <button class="carousel_indicator"></button>
     </div>
@@ -35,8 +55,8 @@
     //Variables.
     const track = document.querySelector('.carousel__track');
     const slides = Array.from(track.children);
-    const nextBTTN = document.querySelector('.carousel__button--rigth');
-    const prevBTTN = document.querySelector('.carousel__button--left');
+    // const nextBTTN = document.querySelector('.carousel__button--rigth');
+    // const prevBTTN = document.querySelector('.carousel__button--left');
     const dotNav = document.querySelector('.carousel_nav');
     const dots = Array.from(dotNav.children);
     const slideSIZE = slides[0].getBoundingClientRect().width;
@@ -47,62 +67,62 @@
     };
 
     slides.forEach(setSlidePOTN);
-
-    //All functions
+    
     const moveToSlide = (track, currentSlides, targetSlide) => {
-        track.style.transform = 'translateX(-' + targetSlide.style.left +')';
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
         currentSlides.classList.remove('currentSlides')
         targetSlide.classList.add('currentSlides')
     }
 
     const updateDots = (currentDot, targetDot) => {
-        currentDot.classList.remove('currentSlides');
-        targettDot.classList.add('currentSlides');
+
+        currentDot.classList.remove('currentSlide');
+        targetDot.classList.add('currentSlide');
     }
 
-    const hideArrowBTTNS = (slides, prevBTTN, nextBTTN, targetIndex) => {
-        if(targetIndex === 0){
-            prevBTTN.classList.add('is-hidden');
-            nextBTTN.classList.remove('is-hidden');
-        }
+    // const hideArrowBTTNS = (slides, prevBTTN, nextBTTN, targetIndex) => {
+    //     if(targetIndex === 0){
+    //         prevBTTN.classList.add('is-hidden');
+    //         nextBTTN.classList.remove('is-hidden');
+    //     }
 
-        else if(targetIndex === slides.length -1){
-            prevBTTN.classList.remove('is-hidden');
-            nextBTTN.classList.add('is-hidden');
-        }
+    //     else if(targetIndex === slides.length -1){
+    //         prevBTTN.classList.remove('is-hidden');
+    //         nextBTTN.classList.add('is-hidden');
+    //     }
 
-        else{
-            prevBTTN.classList.remove('is-hidden');
-            nextBTTN.classList.remove('is-hidden');
-        }
-    }
+    //     else{
+    //         prevBTTN.classList.remove('is-hidden');
+    //         nextBTTN.classList.remove('is-hidden');
+    //     }
+    // }
 
     //Clicking to the previous photo
 
-    prevBTTN.addEventListener('click', e => {
-        const currentSlides = track.querySelector('.currentSlide');
-        const prevSlide = currentSlides.previousElementSibling;
-        const currentDot = dotNav.querySelector('.currentSlide');
-        const prevDot = currentDot.previousElementSibling;
-        const prevIndex = slides.findIndex(slide => slide === prevSlide)
+    //  prevBTTN.addEventListener('click', e => {
+    //     const currentSlides = track.querySelector('.currentSlide');
+    //     const prevSlide = currentSlides.previousElementSibling;
+    //     const currentDot = dotNav.querySelector('.currentSlide');
+    //     const prevDot = currentDot.previousElementSibling;
+    //     const prevIndex = slides.findIndex(slide => slide === prevSlide)
 
-        moveToSlide(track, currentSlides, prevSlide);
-        updateDots(currentDot, prevDot);
-        hideArrowBTTNS(slides, prevBTTN, nextBTTN, prevIndex);
-    })
+    //     moveToSlide(track, currentSlides, prevSlide);
+    //     updateDots(currentDot, prevDot);
+    //     hideArrowBTTNS(slides, prevBTTN, nextBTTN, prevIndex);
+    // })
 
     //Clicking to the next photo.
-    nextBTTN.addEventListener('click', e => {
-        const currentSlides = track.querySelector('.currentSlide');
-        const nextSlide = currentSlides.nextElementSibling;
-        const currentDot = dotNav.querySelector('.currentSlide');
-        const nextDot = currentDot.nextElementSibling;
-        const nextIndex = slides.findIndex(slide => slide === nextSlide)
+    // nextBTTN.addEventListener('click', e => {
+    //     const currentSlides = track.querySelector('.currentSlide');
+    //     const nextSlide = currentSlides.nextElementSibling;
+    //     const currentDot = dotNav.querySelector('.currentSlide');
+    //     const nextDot = currentDot.nextElementSibling;
+    //     const nextIndex = slides.findIndex(slide => slide === nextSlide)
 
-        moveToSlide(track, currentSlides, nextSlide);
-        updateDots(currentDot, nextDot);        
-        hideArrowBTTNS(slides, prevBTTN, nextBTTN, nextIndex);
-    })
+    //     moveToSlide(track, currentSlides, nextSlide);
+    //     updateDots(currentDot, nextDot);        
+    //     hideArrowBTTNS(slides, prevBTTN, nextBTTN, nextIndex);
+    // })
 
     //Updating nav dots.
     dotNav.addEventListener('click', e =>{
@@ -117,7 +137,7 @@
 
         moveToSlide(track, currentSlides, targetSlide);
         updateDots(currentDot, targetDot);
-        hideArrowBTTNS(slides, prevBTTN, nextBTTN, targetIndex);
-    })
+        // hideArrowBTTNS(slides, prevBTTN, nextBTTN, targetIndex);
+    }) 
 </script>
 </body>
