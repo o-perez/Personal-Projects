a1814a29fc75da25c3aeefcfdca370bbc792d0b1
Orlando Perez <56610115+o-perez@users.noreply.github.com>
2023-12-25 11:23:26 -0400

    Starting

---
+++ Portfolio/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>Orlando X Perez Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css" >
+        <!-- Required meta tags-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+        <header>
+            <div id="MySelfPresentation">
+                <img src="Imgs\currentUser.png" alt="Image of myself.">
+                <h1>Orlando X Pérez Varela</h1>
+                 
+            </div>
+        </header>
+        <main>
+            <nav>
+                <div class="navList">
+                    <ul>
+                        <li>About me</li>
+                        <li>Projects</li>
+                        <li>Contact me</li>
+                    </ul>
+                </div>
+            </nav>
+        </main>
+        <footer></footer>
+    </body>
+</html>
