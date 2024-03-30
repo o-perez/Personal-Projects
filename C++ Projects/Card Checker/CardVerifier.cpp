b1eb864c58c2955a14cc890e6a06e3aed7c58761
Orlando Perez <56610115+o-perez@users.noreply.github.com>
2024-03-30 11:52:57 -0400

    Adding Luhn Algorithm
    In this commit the lunh algorithm was added to verify is a card is valid

--- C++ Projects/Card Checker/CardVerifier.cpp
+++ C++ Projects/Card Checker/CardVerifier.cpp
@@ -6,13 +6,68 @@ class CardVerifier {
 private:
 	bool isCommon;
 	bool isReal;
-	bool isFake;
+	bool isFraud;
 
 	std::string type;
 	char firstNumber;
 
 public:
+    void luhnAlg(int cardNumbArrRec[16]) {
+        int evenDigits[8],
+            oddDigits[8],
+            nineAndUpp,
+            evenSum = 0,
+            oddSum = 0,
+            totalSum = 0,
+            modOpResult = 0,
+            cardLastDigit = 0,
+            compModRes = 0;
 
+        //For loop to move between even card number digits.
+        for (int even = 0; even < 8; even = even + 2) {
+            
+            nineAndUpp = cardNumbArrRec[even] * 2;
+            if (nineAndUpp > 9) {
+                nineAndUpp = nineAndUpp - 9;
+                evenDigits[even] = nineAndUpp;
+            }
+
+            else {
+                evenDigits[even] = nineAndUpp;
+            }
+           
+            //evenDigits[even] = cardNumbArrRec[even] * 2;
+        }
+
+        for (int counter3 = 0; counter3 < 8; counter3++) {
+            evenSum = evenSum + evenDigits[counter3];
+        }
+
+        //For loop to move between odd card number digits.
+        for (int odd = 1; odd < 8; odd = odd + 2) {
+            //std::cout << cardNumbArrRec[counter3] << std::endl;
+            oddDigits[odd] = cardNumbArrRec[odd];
+        }
+
+        for (int counter4 = 0; counter4 < 16; counter4++) {
+            oddSum = oddSum + oddDigits[counter4];
+        }
+
+        totalSum = evenSum + oddSum;
+        modOpResult = 10 % totalSum;
+        cardLastDigit = cardNumbArrRec[15];
+        compModRes = modOpResult + cardLastDigit;
+        
+        if (compModRes == 10) {
+            std::cout << "Is a valid card!" << std::endl;
+        }
+
+        else {
+            std::cout << "Is a invalid card!" << std::endl;
+        }
+    }
+
+    //Function that determine if is someone try attack the system.
 	void checkigValidation(std::string cardNumberToRecFunc) {
         char cardNumberArrChar[16],
             currentchar;
@@ -26,46 +81,34 @@ public:
         //For loop used to turn the entered card number into an char array.
 		for (int counter = 0; counter < cardNumberToRecFunc.size(); counter++) {
             currentchar = cardNumberToRecFunc.at(counter);
-            if (!isdigit(currentchar)) {
-                 std::cout << "This card is a fraud!" << std::endl;
-                 cardFraud = true;
-                break;
-            }
+            if (isdigit(currentchar)) {
+                cardNumberArrChar[counter] = currentchar;
+            } 
 
             else {
-                cardNumberArrChar[counter] = cardNumberToRecFunc.at(counter);      
+                cardFraud = true;
+                isFraud = true;
+                std::cout << "System attack detected!" << std::endl;
+                break;
             }
 		}
 
-        /*while (counter < cardNumberToRecFunc.size() || cardFraud != true) {
-            currentchar = cardNumberToRecFunc.at(counter);
-            if (!isdigit(currentchar)) {
-                std::cout << "This card is a fraud!" << std::endl;
-                cardFraud = true;
-            }
+        if (cardFraud ==false) {
 
-            else {
-                cardNumberArrChar[counter] = cardNumberToRecFunc.at(counter);
-                counter++;
-            }
-        }*/
-
-        if (cardFraud == false) {
             //Nested for loop used to turn char array into decimal and save it in an array.
 		    for (int counter1 = 0; counter1 < 16; counter1++) {
                 for (int counter2 = 0; counter2 < 10; counter2++) {
-                    charToInt = (int)digitArrChar[counter2] - 48; //This line is for convert char to int. The number 48 is decimal value of '0'.
-                    cardNumberArrInt[counter1] = charToInt;
+                    if (cardNumberArrChar[counter1] == digitArrChar[counter2]) {
+                        charToInt = (int)digitArrChar[counter2] - 48; //This line is for convert char to int. The number 48 is decimal value of '0'.
+                        cardNumberArrInt[counter1] = charToInt;
+                    }    
                 }
 		    }
 
-            //For loop to move between the card number digits.
-            for (int counter3 = 0; counter3 < 16; counter3++) {
-                std::cout << cardNumberArrInt[counter3] << std::endl;
-            }
-        }
-        
+            luhnAlg(cardNumberArrInt);
+        }       
 	}
+
 	//Function that is on charge of determines which type is the card.
 	void definningTheType(std::string cardNumberToRecFunc) {
 		firstNumber = cardNumberToRecFunc.at(0);
@@ -133,4 +176,3 @@ public:
 		return type;
 	}
 };
-
